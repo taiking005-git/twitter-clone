863777ba3cd0bf180acab1d5312d847f67a407a1
Kolawole Taiwo <taiking005.dev@gmail.com>
2023-01-08 23:26:45 +0100

    added basic feauture

---
+++ post.js
@@ -0,0 +1,35 @@
+export const post = [
+    {
+        tweetID: 1,
+        userID: 1,
+        text: "This is a post 1",
+        img: "./assets/images/post/1.jpg",
+        status: [],
+        retweets: 10,
+        likes: 10,
+    },
+    {
+        tweetID: 2,
+        userID: 1,
+        text: "This is a post 2",
+        img: "./assets/post/2.jpg",
+        status: [],
+        retweets: 10,
+        likes: 10,
+    }
+]
+
+export const users = [{
+    userID: 1,
+    userImg: "./assets/person/pic-1.png",
+    userName: "Weyimi B",
+    userTag: "@_weyimi",
+    userPassword: "",
+    userEmail: "",
+    userBio: "",
+    userLocation: "",
+    followers: [],
+    followings: [],
+    tweets: [],
+    retweets: 0,
+}]
